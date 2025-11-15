53e97aaa11e1a8fadcfe00de8efed5458420fa23
Ralph <rwatts@benchworks.com>
2025-11-15 14:53:20 -0500

    Nov 2025 Update
    Added summary, swapped linkedin for resume, added some BW dev

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import pricesShowcase from '../public/images/prices.webp'
 import { Inter, Noto, Baskervville } from '@next/font/google'
 import { BsInfoCircle } from "@react-icons/all-files/bs/BsInfoCircle";
 import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
+import { FaDownload } from "@react-icons/all-files/fa/FaDownload";
 import { VscActivateBreakpoints } from "@react-icons/all-files/vsc/VscActivateBreakpoints";
 import { VscArrowRight } from "@react-icons/all-files/vsc/VscArrowRight";
 import { GrTools } from "@react-icons/all-files/gr/GrTools";
@@ -27,14 +28,22 @@ export default function Home() {
           <div>
             <h1>Portfolio</h1>
             <h2>Ralph Watts, Web Developer</h2>
+            <p>Full-stack Web Developer specializing in modern JavaScript frameworks, scalable WordPress builds, and marketing technology integrations. Experienced delivering pixel-perfect, high-performance web solutions for enterprise clients.</p>
           </div>
-          <div>
-            <a
+          <div className={styles.resumeTopLink}>
+            {/*<a
               href="https://www.linkedin.com/in/ralphwatts/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Button size="lg" auto rounded flat color="secondary">Linked<FaLinkedin /></Button>
+            </a>*/}
+            <a
+              href="/pdfs/Ralph Watts' Resume Extended.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button size="lg" auto rounded flat color="secondary">Resume&nbsp;<FaDownload /></Button>
             </a>
           </div>
         </section>
@@ -60,7 +69,7 @@ export default function Home() {
                     placement: 'bottom-left',
                     color: '#fff'
                   }}>
-                    <p>HTML, CSS, Javascript, React, PHP and Wordpress expert.<br></br>8+ years of professional web development experience across many frameworks and CMS'</p>
+                    <p>HTML, CSS, Javascript, React, PHP and WordPress expert.<br></br>8+ years of professional web development experience across many frameworks and CMS'</p>
                   </Popover.Content>
                 </Popover>
               </li>
@@ -137,13 +146,15 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.portfoloimg}
-                src={pricesShowcase}
-                alt="Coinme.com/prices screenshot"
-                width={192}
-                height={400}
-              />
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src={pricesShowcase}
+                  alt="Coinme.com/prices screenshot"
+                  width={192}
+                  height={400}
+                />
+              </div>
               <h2 className={inter.className}>
                 Coinme.com/prices <span><VscArrowRight /></span>
               </h2>
@@ -158,14 +169,16 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.portfoloimg}
-                src="/images/map.webp"
-                alt="Coinme store locator screenshot"
-                width={481}
-                height={400}
-                priority
-              />
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/map.webp"
+                  alt="Coinme store locator screenshot"
+                  width={481}
+                  height={400}
+                  priority
+                />
+              </div>
               <h2 className={inter.className}>
                 Store locator<span><VscArrowRight /></span>
               </h2>
@@ -180,19 +193,115 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.portfoloimg}
-                src="/images/btc-price.webp"
-                alt="Bitcoin price history page screenshot"
-                width={192}
-                height={400}
-                priority
-              />
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/btc-price.webp"
+                  alt="Bitcoin price history page screenshot"
+                  width={192}
+                  height={400}
+                  priority
+                />
+              </div>
               <h2 className={inter.className}>
                 Bitcoin price history page <span><VscArrowRight /></span>
               </h2>
               <p className={inter.className}>
-                I developed and designed the crypto price history pages on coinme.com.
+                Developed and designed the crypto price history pages on coinme.com.
+              </p>
+            </a>
+
+
+            <a
+              href="https://cryptoliteracy.org/quiz/"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/cortrophin.webp"
+                  alt="cortrophin.com screenshot"
+                  width={482}
+                  height={400}
+                  priority
+                />
+              </div>
+              <h2 className={inter.className}>
+                Cortrophin.com <span><VscArrowRight /></span>
+              </h2>
+              <p className={inter.className}>
+                Prescription medication website built in NextJS.
+              </p>
+            </a>
+            <a
+              href="https://whitehawk.com"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/whitehawk.webp"
+                  alt="whitehawk.com screenshot"
+                  width={400}
+                  height={400}
+                  priority
+                />
+              </div>
+              <h2 className={inter.className}>
+                Whitehawk.com <span><VscArrowRight /></span>
+              </h2>
+              <p className={inter.className}>
+                Biotechnology company website built in WordPress.
+              </p>
+            </a>
+            <a
+              href="https://rhuemagen.com"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/rheumagen.webp"
+                  alt="rhuemagen.com screenshot"
+                  width={400}
+                  height={400}
+                  priority
+                />
+              </div>
+              <h2 className={inter.className}>
+                Rheumagen.com <span><VscArrowRight /></span>
+              </h2>
+              <p className={inter.className}>
+                Biotechnology company website built in WordPress.
+              </p>
+            </a>
+            <a
+              href="https://coinme.com/learn/"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/learn.webp"
+                  alt="Coinme.com/learn screenshot"
+                  width={482}
+                  height={400}
+                  priority
+                />
+              </div>
+              <h2 className={inter.className}>
+                Coinme.com/learn <span><VscArrowRight /></span>
+              </h2>
+              <p className={inter.className}>
+                Page design and development for coinme.com/learn, contributed to articles and content ideas.
               </p>
             </a>
             <a
@@ -201,19 +310,21 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.portfoloimg}
-                src="/images/quiz-ipad.webp"
-                alt="cryptoliteracy.org/quiz screenshot"
-                width={482}
-                height={400}
-                priority
-              />
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/quiz-ipad.webp"
+                  alt="cryptoliteracy.org/quiz screenshot"
+                  width={482}
+                  height={400}
+                  priority
+                />
+              </div>
               <h2 className={inter.className}>
                 Cryptoliteracy.org/quiz <span><VscArrowRight /></span>
               </h2>
               <p className={inter.className}>
-                An interactive quiz built in Wordpress.
+                An interactive quiz built in WordPress.
               </p>
             </a>
 
@@ -223,14 +334,16 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.portfoloimg}
-                src="/images/earthandclay.webp"
-                alt="earthandclaysoaps.com screenshot"
-                width={192}
-                height={400}
-                priority
-              />
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/earthandclay.webp"
+                  alt="earthandclaysoaps.com screenshot"
+                  width={192}
+                  height={400}
+                  priority
+                />
+              </div>
               <h2 className={inter.className}>
                 EarthandClaySoaps.com<span><VscArrowRight /></span>
               </h2>
@@ -239,47 +352,29 @@ export default function Home() {
               </p>
             </a>
 
-            <a
-              href="https://coinme.com/learn/"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.portfoloimg}
-                src="/images/learn.webp"
-                alt="Coinme.com/learn screenshot"
-                width={482}
-                height={400}
-                priority
-              />
-              <h2 className={inter.className}>
-                Coinme.com/learn <span><VscArrowRight /></span>
-              </h2>
-              <p className={inter.className}>
-                Check out this page design and price ticker.
-              </p>
-            </a>
-            
+
+
             <a
               href="https://nextjs-template.ralphjwatts.com/"
               className={styles.card}
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.portfoloimg}
-                src="/images/next-temp.webp"
-                alt="NextJS Starter Template"
-                width={482}
-                height={400}
-                priority
-              />
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/next-temp.webp"
+                  alt="NextJS Starter Template"
+                  width={482}
+                  height={400}
+                  priority
+                />
+              </div>
               <h2 className={inter.className}>
                 NextJS Starter Template <span><VscArrowRight /></span>
               </h2>
               <p className={inter.className}>
-              This is a NextJS starter template I built to practice Next and React. I used NextJS, React, SCSS and React Icons to build this.
+                A NextJS starter template I built to practice Next and React. I used NextJS, React, SCSS and React Icons to build this.
               </p>
             </a>
 
@@ -289,14 +384,16 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.portfoloimg}
-                src="/images/weather-app.webp"
-                alt="Weather React App"
-                width={192}
-                height={400}
-                priority
-              />
+              <div className={styles.imgWrapper}>
+                <Image
+                  className={styles.portfoloimg}
+                  src="/images/weather-app.webp"
+                  alt="Weather React App"
+                  width={192}
+                  height={400}
+                  priority
+                />
+              </div>
               <h2 className={inter.className}>
                 Weather React App <span><VscArrowRight /></span>
               </h2>
@@ -318,39 +415,42 @@ export default function Home() {
         <section className={styles.hero}>
           <div className={styles.footer}>
             <h1>Contact</h1>
-            <p>Message me on <a
-              href="https://www.linkedin.com/in/ralphwatts/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >Linkedin</a>.</p>
+            <p>Contact info on resume.</p>
           </div>
           <div>
-            <a
+            {/*<a
               href="https://www.linkedin.com/in/ralphwatts/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Button size="lg" auto rounded flat color="secondary">Linked<FaLinkedin /></Button>
+            </a>*/}
+            <a
+              href="/pdfs/Ralph Watts' Resume Extended.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button size="lg" auto rounded flat color="secondary">Resume&nbsp;<FaDownload /></Button>
             </a>
           </div>
         </section>
         <section className={styles.copyright}>
           <div>
-            <p><GrTools /> Built with  
+            <p><GrTools /> Built with
               <a
-              href="https://nextjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              > NextJS</a>, 
+                href="https://nextjs.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              > NextJS</a>,
               <a
-              href="https://nextui.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              > NextUI</a> and 
+                href="https://nextui.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              > NextUI</a> and
               <a
-              href="https://react-icons.github.io/react-icons"
-              target="_blank"
-              rel="noopener noreferrer"
+                href="https://react-icons.github.io/react-icons"
+                target="_blank"
+                rel="noopener noreferrer"
               > React Icons</a></p>
           </div>
         </section>
